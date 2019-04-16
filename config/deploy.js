f04fe0a3c82a11a7e7955ddcbdb5afb114979103
Pat O'Callaghan <pat@intercom.io>
2019-04-16 23:04:16 +0100

    Add Sentry

---
+++ config/deploy.js
@@ -0,0 +1,40 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(deployTarget) {
+  let ENV = {
+    build: {}
+    // include other plugin configuration that applies to all deploy targets here
+  };
+
+  ENV.sentry = {
+    // the URL or CDN your js assets are served from
+    publicUrl: 'https://localhost:4200',
+    // the sentry install you're using, https://sentry.io for hosted accounts
+    sentryUrl: 'https://sentry.io',
+    sentryOrganizationSlug: 'patocallaghan',
+    sentryProjectSlug: 'test-app',
+    
+    sentryBearerApiKey: process.env.SENTRY_TOKEN
+  }
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
